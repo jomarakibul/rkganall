4bef9018aaa0676d3840822a3a70ce8748c33246
jomarakibul <jomarakibul@gmail.com>
2026-07-28 01:13:08 +0300

    Add or update user admin

--- ngf.js
+++ ngf.js
@@ -1,1 +1,16 @@
-nhjf
+{
+  "admin": [
+    {
+      "User": "admin",
+      "app_name": "rsg",
+      "status": "online",
+      "password": "RA@2026",
+      "upload_type": "github",
+      "cloud_upload_visible": true,
+      "message": "ggx",
+      "domain": "gxgvv",
+      "post_api": "xvg",
+      "get_api": "vxv"
+    }
+  ]
+}
